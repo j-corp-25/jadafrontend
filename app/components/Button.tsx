7caf6020b1ebafb8e4d0e6c02daebd5de4004ebc
Jordy <jordycorporan95@gmail.com>
2024-02-08 04:59:56 -0500

    little details

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -6,12 +6,15 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, additionalClasses }) => {
   return (
+
+
     <button
-      className={`bg-jada-purple-800 text-white px-4 py-2 rounded-md text-lg font-semibold hover:bg-jada-pink transition-colors shadow-2xl ${additionalClasses}`}
+      className={`bg-jada-purple-800 text-white px-4 py-2 rounded-md text-lg font-semibold  hover:bg-jada-pink ${additionalClasses}`}
       onClick={onClick}
-    >
+      >
       {text}
     </button>
+
   )
 }
 
