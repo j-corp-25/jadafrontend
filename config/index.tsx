78c67e19148992979d556c6821cc450aec454028
Jordy <jordycorporan95@gmail.com>
2024-02-23 18:46:46 -0500

    change port

--- config/index.tsx
+++ config/index.tsx
@@ -1,7 +1,1 @@
-// Define your URLs
-const url1 = process.env.NEXT_PUBLIC_API_URL;
-const url2 = 'http://localhost:1338';
-const url3 = 'http://localhost:1337';
-
-
-export const API_URL = url1 || url2 || url3;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1338'
